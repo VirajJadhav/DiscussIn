cec7a4e5a8bdffa87516fb469fc5c2d80b53be85
AbhishekHonmute <honmuteabhishek@gmail.com>
2021-04-09 16:30:50 +0530

    profile card added

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as DashBoard } from "./DashBoard";
 export { default as Login } from "./Auth/Login";
 export { default as Signup } from "./Auth/Signup";
 export { default as Room } from "./Room";
+export { default as Profile } from "./Profile";
